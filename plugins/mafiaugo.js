173fd38435c4848ff734cac570aee4a44fc72ee1
Meijer,L. (Lucas) <l.meijer6@students.uu.nl>
2023-07-05 08:30:36 +0200

    Update UGO message

--- plugins/mafiaugo.js
+++ plugins/mafiaugo.js
@@ -36,12 +36,12 @@ class MafiaUGO extends Rooms.RoomGame {
 	disable() {
 		if (!this.enabled) return this.sendRoom(`UGO mode already disabled`);
 		this.enabled = false;
-		this.sendRoom(`The mafia cooldown was disabled`);
+		this.sendRoom(`Mafia UGO was disabled`);
 	}
 	enable() {
 		if (this.enabled) return this.sendRoom(`UGO mode already enabled`);
 		this.enabled = true;
-		this.sendRoom(`The mafia cooldown was enabled`);
+		this.sendRoom(`Mafia UGO was enabled`);
 	}
 	hostGame() {
 		this.sendRoom(`/mafia host dewbott`);
